075db984e43363ee3d5683e18cf5f588eabf1412
Karol Kotwica <karol.kotwica@britenet.com.pl>
2018-01-30 17:26:38 +0100

    test bash

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+mkdir migration_tool;
+if [ ! -f /migration_tool/ant-salesforce.jar ]; then
+   cd migration_tool;
+   wget https://gs0.salesforce.com/dwnld/SfdcAnt/salesforce_ant_41.0.zip;
+   unzip salesforce_ant_41.0.zip
+   pwd
+   cd ..
+   pwd
+else
+    echo "File found"
+fi
