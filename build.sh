908312f05c4ff43adfcecd05b5db6a955ac8cafa
Karol Kotwica <karol.kotwica@britenet.com.pl>
2018-01-30 17:37:45 +0100

    test not if

--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 mkdir migration_tool;
 if [ ! -f /migration_tool/ant-salesforce.jar ]; then
-   cd migration_tool;
-   wget https://gs0.salesforce.com/dwnld/SfdcAnt/salesforce_ant_41.0.zip;
-   unzip salesforce_ant_41.0.zip
-   pwd
-   cd ..
-   pwd
+	echo "File not found"
+   # cd migration_tool;
+   # wget https://gs0.salesforce.com/dwnld/SfdcAnt/salesforce_ant_41.0.zip;
+   # unzip salesforce_ant_41.0.zip
+   # pwd
+   # cd ..
+   # pwd
 else
     echo "File found"
 fi
