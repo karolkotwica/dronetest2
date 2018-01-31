1c316f59330fa2fbef6a43913b2dc1d23163f483
Karol Kotwica <karol.kotwica@britenet.com.pl>
2018-01-31 09:08:30 +0100

    testing why migration tool is still downloaded even if it exists

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 mkdir migration_tool;
 if [ ! -f /migration_tool/ant-salesforce.jar ]; then	
-    cd migration_tool;
-    wget https://gs0.salesforce.com/dwnld/SfdcAnt/salesforce_ant_41.0.zip;
+    cd migration_tool
+    pwd 
+    echo 'file should exists!!!!'
+    ls
+    wget https://gs0.salesforce.com/dwnld/SfdcAnt/salesforce_ant_41.0.zip
     unzip salesforce_ant_41.0.zip
     pwd
     cd ..
