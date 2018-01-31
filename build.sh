b9f5663e1c5ecf6c57325182e32b37476ec71bbc
Karol Kotwica <karol.kotwica@britenet.com.pl>
2018-01-31 09:15:35 +0100

    testing simpler cases to check if i can tell if file is available

--- build.sh
+++ build.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
-mkdir migration_tool;
-if [ ! -f /migration_tool/ant-salesforce.jar ]; then	
-    cd migration_tool
-    pwd 
-    echo 'file should exists!!!!'
-    ls
-    wget https://gs0.salesforce.com/dwnld/SfdcAnt/salesforce_ant_41.0.zip
-    unzip salesforce_ant_41.0.zip
-    pwd
-    cd ..
-	pwd
+mkdir -p migration_tool
+cd migration_tool
+pwd
+ls
+
+if [ -f ant-salesforce.jar ]; then
+	echo 'file exists!!!!'
 else
-    echo "Migration tool file found"
-fi
+	echo 'file not exists!!!!'	
+if
+
+#if [ ! -f /migration_tool/ant-salesforce.jar ]; then
+#    cd migration_tool
+#    pwd 
+#    echo 'file should exists!!!!'
+#    ls
+#    wget https://gs0.salesforce.com/dwnld/SfdcAnt/salesforce_ant_41.0.zip
+#    unzip salesforce_ant_41.0.zip
+#    pwd
+#    cd ..
+#	pwd
+#else
+#   echo "Migration tool file found"
+#fi
