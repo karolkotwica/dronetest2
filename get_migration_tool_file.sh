e438f3bc24ee081b6c9b403fbccbb561b9c75b78
Karol Kotwica <karol.kotwica@britenet.com.pl>
2018-01-31 10:32:22 +0100

    returing to separate bash file

--- get_migration_tool_file.sh
+++ get_migration_tool_file.sh
@@ -2,15 +2,11 @@
 
 mkdir -p migration_tool
 cd migration_tool
-pwd
-ls
 
 if [ -f ant-salesforce.jar ]; then
 	echo 'file exists!!!!';
 else
     wget https://gs0.salesforce.com/dwnld/SfdcAnt/salesforce_ant_41.0.zip
     unzip salesforce_ant_41.0.zip
-    pwd
     cd ..
-	pwd
 fi
