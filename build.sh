b9ebe3b30d8b06a5fd499c5cd1b95ee8b9ad503a
Karol Kotwica <karol.kotwica@britenet.com.pl>
2018-01-31 08:46:22 +0100

    cache test

--- build.sh
+++ build.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 mkdir migration_tool;
-if [ ! -f /migration_tool/ant-salesforce.jar ]; then
-	echo "File not found"
-   # cd migration_tool;
-   # wget https://gs0.salesforce.com/dwnld/SfdcAnt/salesforce_ant_41.0.zip;
-   # unzip salesforce_ant_41.0.zip
-   # pwd
-   # cd ..
-   # pwd
+if [ ! -f /migration_tool/ant-salesforce.jar ]; then	
+    cd migration_tool;
+    wget https://gs0.salesforce.com/dwnld/SfdcAnt/salesforce_ant_41.0.zip;
+    unzip salesforce_ant_41.0.zip
+    pwd
+    cd ..
+	pwd
 else
-    echo "File found"
+    echo "Migration tool file found"
 fi
