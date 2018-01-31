28a32efe67e985ed5a78bd2501d9675ceb202bea
Karol Kotwica <karol.kotwica@britenet.com.pl>
2018-01-31 09:18:05 +0100

    and next test

--- build.sh
+++ build.sh
@@ -9,17 +9,3 @@ if [ -f ant-salesforce.jar ]; then
 else
 	echo 'file not exists!!!!'	
 if
-
-#if [ ! -f /migration_tool/ant-salesforce.jar ]; then
-#    cd migration_tool
-#    pwd 
-#    echo 'file should exists!!!!'
-#    ls
-#    wget https://gs0.salesforce.com/dwnld/SfdcAnt/salesforce_ant_41.0.zip
-#    unzip salesforce_ant_41.0.zip
-#    pwd
-#    cd ..
-#	pwd
-#else
-#   echo "Migration tool file found"
-#fi
