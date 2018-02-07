358b480f723e50a8c3bfd575159c9415c182a45e
Karol Kotwica <karol.kotwica@britenet.com.pl>
2018-02-07 15:27:34 +0100

    env variables test

---
+++ global.js
@@ -0,0 +1,13 @@
+const test1 = process.env.SF_PASS;
+const test2 = process.env.SF_LOGIN;
+const test3 = process.env.SF_TEST;
+
+module.exports = {
+#    'default': { // 'default' environnement parameters
+#        searchTerm : 'nightwatch',
+#        movieName : 'Night Watch'
+#    }
+     test1 = test1,
+     test2 = test2,
+     test3 = test3,
+};
--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,8 @@
 module.exports = {
   tags: ['google'],
   'Demo test Google' : function (client) {
-        console.log('login to = ' + login);
+        console.log('test1 jest = ' + test1);
+        console.log('test2 jest = ' + test2);
+        console.log('test3 jest = ' + test3);
   }
 };
