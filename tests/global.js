443316ee69457ca00527ab544e663dbc324a1095
Karol Kotwica <karol.kotwica@britenet.com.pl>
2018-02-07 15:52:54 +0100

    adjustement in global.js

--- tests/global.js
+++ tests/global.js
@@ -3,10 +3,6 @@ const test2 = process.env.SF_LOGIN;
 const test3 = process.env.SF_TEST;
 
 module.exports = {
-#    'default': { // 'default' environnement parameters
-#        searchTerm : 'nightwatch',
-#        movieName : 'Night Watch'
-#    }
      test1 = test1,
      test2 = test2,
      test3 = test3,
