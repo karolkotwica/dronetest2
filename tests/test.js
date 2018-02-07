08fc660b41d08b02276890bd2171a269c805883f
Karol Kotwica <karol.kotwica@britenet.com.pl>
2018-02-07 17:05:13 +0100

    wrong context

--- tests/test.js
+++ tests/test.js
@@ -1,8 +1,8 @@
 module.exports = {
   tags: ['google'],
   'Demo test Google' : function (client) {
-        console.log('test1 jest = ' + browser.globals.test1);
-        console.log('test2 jest = ' + browser.globals.test2);
-        console.log('test3 jest = ' + browser.globals.test3);
+        console.log('test1 jest = ' + client.globals.test1);
+        console.log('test2 jest = ' + client.globals.test2);
+        console.log('test3 jest = ' + client.globals.test3);
   }
 };
