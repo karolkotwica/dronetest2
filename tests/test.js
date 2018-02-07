247df00c41f5b0c579a67e6581c08efb76bb3491
Karol Kotwica <karol.kotwica@britenet.com.pl>
2018-02-07 16:54:32 +0100

    adjustments to global variables

--- tests/test.js
+++ tests/test.js
@@ -1,8 +1,8 @@
 module.exports = {
   tags: ['google'],
   'Demo test Google' : function (client) {
-        console.log('test1 jest = ' + test1);
-        console.log('test2 jest = ' + test2);
-        console.log('test3 jest = ' + test3);
+        console.log('test1 jest = ' + browser.globals.test1);
+        console.log('test2 jest = ' + browser.globals.test2);
+        console.log('test3 jest = ' + browser.globals.test3);
   }
 };
