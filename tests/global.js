028fcc8f5905fa5916949ff031fcf2218dffeefd
Karol Kotwica <karol.kotwica@britenet.com.pl>
2018-02-07 16:25:30 +0100

    new adjustements

--- tests/global.js
+++ tests/global.js
@@ -3,7 +3,7 @@ const test2 = process.env.SF_LOGIN;
 const test3 = process.env.SF_TEST;
 
 module.exports = {
-     test1 = test1,
-     test2 = test2,
-     test3 = test3,
+     test1 : test1,
+     test2 : test2,
+     test3 : test3
 };
