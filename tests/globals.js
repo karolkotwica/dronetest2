40772d9d933193023b4b8a8ba6ff413319fe63b5
Karol Kotwica <karol.kotwica@britenet.com.pl>
2018-02-08 10:33:55 +0100

    test

--- tests/globals.js
+++ tests/globals.js
@@ -1,9 +1,9 @@
-const test1 = process.env.SF_PASS;
-const test2 = process.env.SF_LOGIN;
+const username = process.env.SF_LOGIN;
+const pass = process.env.SF_PASS;
 const test3 = process.env.SF_TEST;
 
 module.exports = {
-     test1: test1,
-     test2: test2,
+     username: username,
+     pass: pass,
      test3: test3
 };
--- tests/test.js
+++ tests/test.js
@@ -1,8 +1,10 @@
 module.exports = {
   tags: ['google'],
   'Demo test Google' : function (client) {
-        console.log('test1 jest = ' + client.globals.test1);
-        console.log('test2 jest = ' + client.globals.test2);
+        console.log('test1 jest = ' + client.globals.username);
+        console.log('test2 jest = ' + client.globals.pass);
         console.log('test3 jest = ' + client.globals.test3);
+        console.log('test1 jest = ' + username);
+        console.log('test2 jest = ' + pass);
   }
 };
