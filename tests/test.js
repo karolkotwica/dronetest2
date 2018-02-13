0cb6b8bf6a8c005d3a8980d0be6187a6c96c6e85
Karol Kotwica <karol.kotwica@britenet.com.pl>
2018-02-13 10:08:58 +0100

    removing test

--- tests/test.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  tags: ['google'],
-  'Demo test Google' : function (client) {
-        console.log('test1 jest = ' + client.globals.username);
-        console.log('test2 jest = ' + client.globals.pass);
-        console.log('test3 jest = ' + client.globals.test3);
-  }
-};
