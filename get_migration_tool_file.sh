7f2335a776e28a646740bb6bfeb12a501159effd
Karol Kotwica <karol.kotwica@britenet.com.pl>
2018-02-15 11:22:34 +0100

    wget download show

--- get_migration_tool_file.sh
+++ get_migration_tool_file.sh
@@ -4,7 +4,7 @@ mkdir -p migration_tool
 cd migration_tool
 
 if [ -f ant-salesforce.jar ]; then
-        wget http://www.sforce.britenet.com.pl/wp-content/themes/salesforce/js/jquery-2.1.1.min.js
+        wget https://test.salesforce.com/img/logo198.png
 	# echo 'file exists!!!!';
 else
     wget https://gs0.salesforce.com/dwnld/SfdcAnt/salesforce_ant_41.0.zip
