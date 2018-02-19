3f182633a8171320f2b702abb8275fddc82f4304
Karol Kotwica <karol.kotwica@britenet.com.pl>
2018-02-19 17:05:46 +0100

    testing checking for changes

--- get_migration_tool_file.sh
+++ get_migration_tool_file.sh
@@ -4,8 +4,7 @@ mkdir -p migration_tool
 cd migration_tool
 
 if [ -f ant-salesforce.jar ]; then
-        wget https://test.salesforce.com/img/logo198.png
-	# echo 'file exists!!!!';
+	echo 'file exists!!!!';
 else
     wget https://gs0.salesforce.com/dwnld/SfdcAnt/salesforce_ant_41.0.zip
     unzip salesforce_ant_41.0.zip
