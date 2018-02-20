0325c288776d7519dc1199a051ef87bbf581d618
Karol Kotwica <karol.kotwica@britenet.com.pl>
2018-02-20 11:37:59 +0100

    testing if and grep

--- prof_backup.sh
+++ prof_backup.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
 
-if git status --porcelain | grep "profiles" then
-	echo 'Profiles has changed';
-	# git add profiles/*
-	# git commit -m "test commit from drone [CI SKIP]"
-	# git push origin master
+if git status --porcelain | grep "profiles" 
+then
+	git add profiles/*
+	git commit -m "backup of changed profiles [CI SKIP]"
+	git push origin master
 else
-	echo 'There are no changes to commit';
+	echo 'There are no changes to commit'	
 fi
-
-#git status --porcelain
-#ls
-#git add profiles/*
-#git status --porcelain
-#git status
-#git commit -m "test commit from drone [CI SKIP]"
-#git log
-#- git push origin master
