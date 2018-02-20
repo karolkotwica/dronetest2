ef2e44260552d241b1025584d356b4e8d996f695
Karol Kotwica <karol.kotwica@britenet.com.pl>
2018-02-20 11:47:41 +0100

    testing again

--- prof_backup.sh
+++ prof_backup.sh
@@ -2,9 +2,13 @@
 
 if git status --porcelain | grep "profiles" 
 then
+	echo 'inside 1'
 	git add profiles/*
 	git commit -m "backup of changed profiles [CI SKIP]"
-	git push origin master
+	git push origin master	
 else
 	echo 'There are no changes to commit'	
+	echo 'inside 2'
 fi
+
+echo 'inside 3'
