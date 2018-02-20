71da87d309f3688e1b5650c9ba7fabd4d9f9c029
Karol Kotwica <karol.kotwica@britenet.com.pl>
2018-02-20 12:01:59 +0100

    testing if condition

--- get_migration_tool_file.sh
+++ get_migration_tool_file.sh
@@ -3,9 +3,9 @@
 mkdir -p migration_tool
 cd migration_tool
 ls
-if [ -e ant-salesforce.jar ]
+if [ -e ant-salesforce.jar ];
 then
-	echo 'file exists!!!!';
+	echo 'file exists!!!!'
 else
     wget https://gs0.salesforce.com/dwnld/SfdcAnt/salesforce_ant_41.0.zip
     unzip salesforce_ant_41.0.zip
