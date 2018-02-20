c41ccc91dd708668c86bcd1024afe821aefecbad
Karol Kotwica <karol.kotwica@britenet.com.pl>
2018-02-20 10:45:17 +0100

    error in if condition

--- prof_backup.sh
+++ prof_backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if[ git status --porcelain | grep "profiles" ]; then
+if [ git status --porcelain | grep "profiles" ]; then
 	echo 'Profiles has changed';
 	# git add profiles/*
 	# git commit -m "test commit from drone [CI SKIP]"
