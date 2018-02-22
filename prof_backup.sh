15174eaa934abf24278b2bda42c416faebb5fc42
Karol Kotwica <karol.kotwica@britenet.com.pl>
2018-02-22 09:56:23 +0100

    testing

--- prof_backup.sh
+++ prof_backup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 git status --porcelain
-git add profiles/*
-git commit -m "Autocommit profiles from DEV environment [CI SKIP]"
+# git add profiles/*
+# git commit -m "Autocommit profiles from DEV environment [CI SKIP]"
 git fetch origin master
 git merge origin/master
-git push origin master
+# git push origin master
