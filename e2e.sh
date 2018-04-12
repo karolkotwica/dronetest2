71017d6dddda8bda0ce6fe42bf11d83286100e06
Karol Kotwica <karol.kotwica@britenet.com.pl>
2018-04-12 17:52:46 +0200

    testing with former error redir

--- e2e.sh
+++ e2e.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 exec 3>&1
-nightwatch --verbose tests 2>&1 >&3 | tee -a output.html
+#nightwatch --verbose tests 2>&1 >&3 | tee -a output.html
+nightwatch --verbose tests 2>> output.html
 exit ${PIPESTATUS[0]}
