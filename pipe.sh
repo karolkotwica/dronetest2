59c121ad500dce944b628ec6eab0d5f02c42b9a3
Karol Kotwica <karol.kotwica@britenet.com.pl>
2018-04-12 12:32:30 +0200

    tests

--- pipe.sh
+++ pipe.sh
@@ -3,7 +3,7 @@
 # we will know if the pipe fails, but if both 'command' and 'tee' fail, we will
 # receive the exit code from 'tee'
 echo 'starting'
-set -o pipefail
-cat x| sed 's///'
+# set -o pipefail
+cat x| foo
 echo $?
-echo $${DASHUSERNAME}
+echo $PIPESTATUS
