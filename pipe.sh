068e1da2b2593ef935fcfaf9f93ba0e83a59e0cb
Karol Kotwica <karol.kotwica@britenet.com.pl>
2018-04-12 14:59:34 +0200

    testing for final version hopefuly

--- pipe.sh
+++ pipe.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# we will know if the pipe fails, but if both 'command' and 'tee' fail, we will
-# receive the exit code from 'tee'
-echo 'starting'
-# set -o pipefail
-exec 3>&1;
-ant -lib migration_tool/ant-salesforce.jar -Dusername=$SFUSR -Dpassword=$SFPSS 2>&1 >&3 | tee -a output.html | ls
-echo ${PIPESTATUS[@]};
-${PIPESTATUS[0]}
-echo 'testtt';
-echo "${PIPESTATUS[0]}"
-exit 1
+exec 3>&1
+ant -lib migration_tool/ant-salesforce.jar -Dusername=$SFUSR -Dpassword=$SFPSS 2>&1 >&3 | tee -a output.html
+exit ${PIPESTATUS[0]}
