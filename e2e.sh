f2bef8f68260fd66136b74d23276be887ab8a624
Karol Kotwica <karol.kotwica@britenet.com.pl>
2018-04-12 19:42:49 +0200

    removing verbose option in e2e

--- e2e.sh
+++ e2e.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-#nightwatch --verbose tests 2>&1 >&3 | tee -a output.html
-nightwatch --verbose tests | tee -a output.html
+# use --verbose option for debugging purposes
+nightwatch tests | tee -a output.html
 exit ${PIPESTATUS[0]}
