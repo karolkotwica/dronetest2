8cdddfabc559fb9297c496ad05b3941b396abc7f
Karol Kotwica <karol.kotwica@britenet.com.pl>
2018-04-12 14:38:26 +0200

    next try

--- pipe.sh
+++ pipe.sh
@@ -4,8 +4,12 @@
 # receive the exit code from 'tee'
 echo 'starting'
 # set -o pipefail
-exec 3>&1
+exec 3>&1;
 ant -lib migration_tool/ant-salesforce.jar -Dusername=$SFUSR -Dpassword=$SFPSS 2>&1 >&3 | tee -a output.html | foo | ls;
-echo ${PIPESTATUS[*]}
-echo ${PIPESTATUS[3]}
+echo ${PIPESTATUS[*]};
+echo 'testtt';
+echo "${PIPESTATUS[0]}"
+echo "${PIPESTATUS[1]}"
+echo "${PIPESTATUS[2]}"
+echo "${PIPESTATUS[3]}"
 exit 1
