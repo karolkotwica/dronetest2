4ee3bbbf6c02c162e872738bff6a97c8f7c939fb
Karol Kotwica <karol.kotwica@britenet.com.pl>
2018-04-12 14:05:36 +0200

    checking order inside pipestatus

--- pipe.sh
+++ pipe.sh
@@ -5,7 +5,10 @@
 echo 'starting'
 # set -o pipefail
 exec 3>&1
-ant -lib migration_tool/ant-salesforce.jar -Dusername=$SFUSR -Dpassword=$SFPSS 2>&1 >&3 | tee -a output.html
+ant -lib migration_tool/ant-salesforce.jar -Dusername=$SFUSR -Dpassword=$SFPSS 2>&1 >&3 | tee -a output.html | foo | ls
 echo ${PIPESTATUS[*]}
 echo ${PIPESTATUS[0]}
+echo ${PIPESTATUS[1]}
+echo ${PIPESTATUS[2]}
+echo ${PIPESTATUS[3]}
 exit 1
