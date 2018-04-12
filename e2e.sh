a7e511d780a8fdcb809b1657c23612ae0166161b
Karol Kotwica <karol.kotwica@britenet.com.pl>
2018-04-12 18:34:03 +0200

    it's stdout alright, test using <pre><code> tags

--- e2e.sh
+++ e2e.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-exec 3>&1
 #nightwatch --verbose tests 2>&1 >&3 | tee -a output.html
-nightwatch --verbose tests 1>> output.html
+nightwatch --verbose tests | tee -a output.html
 exit ${PIPESTATUS[0]}
