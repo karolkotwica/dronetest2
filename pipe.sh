b7913276d221a13c71d7e81c0c26e903acc4589e
Karol Kotwica <karol.kotwica@britenet.com.pl>
2018-04-12 14:49:35 +0200

    testing pipestatus

--- pipe.sh
+++ pipe.sh
@@ -5,11 +5,9 @@
 echo 'starting'
 # set -o pipefail
 exec 3>&1;
-ant -lib migration_tool/ant-salesforce.jar -Dusername=$SFUSR -Dpassword=$SFPSS 2>&1 >&3 | tee -a output.html | foo | ls;
-echo ${PIPESTATUS[*]};
+ant -lib migration_tool/ant-salesforce.jar -Dusername=$SFUSR -Dpassword=$SFPSS 2>&1 >&3 | tee -a output.html | ls
+echo ${PIPESTATUS[@]};
+${PIPESTATUS[0]}
 echo 'testtt';
 echo "${PIPESTATUS[0]}"
-echo "${PIPESTATUS[1]}"
-echo "${PIPESTATUS[2]}"
-echo "${PIPESTATUS[3]}"
 exit 1
