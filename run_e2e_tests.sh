0962930fbcb93284f1af389d4081c89e8b9901d7
Karol Kotwica <karol.kotwica@britenet.com.pl>
2018-05-14 12:54:07 +0200

    refresh function gets object instead of boolean

--- run_e2e_tests.sh
+++ run_e2e_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # use --verbose option for debugging purposes
-nightwatch tests/tests | tee -a output.html
+nightwatch --verbose tests/tests | tee -a output.html
 exit ${PIPESTATUS[0]}
--- tests/tests/PC_GoToObjectList.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    PC_GoToObject : function (client) {    	
-        client
-        	.loginToTenfold()
-            .goToDestination(true, 'Customers')
-            .refreshUntilElementVisible("xpath", "//a[@title='Customers']")
-            .end();
-        }
-  };
