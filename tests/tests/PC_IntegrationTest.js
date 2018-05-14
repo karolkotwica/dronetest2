0707b819c655e00872762c57015256c3f113ec11
Karol Kotwica <karol.kotwica@britenet.com.pl>
2018-05-14 16:05:29 +0200

    new version of goToDestination function must be called to be tested

--- tests/tests/PC_IntegrationTest.js
+++ tests/tests/PC_IntegrationTest.js
@@ -2,14 +2,19 @@ module.exports = {
 
     PC_IntegrationsStart : function(client) {
         client.loginToTenfold();
+        client.goToDestination(false, 'Integrations');
+        
+        /*
         client
             .refreshUntilElementVisible("xpath", "//nav[@class='forceCommunityNavigationMenu']")
             .click("//a[@title = 'Integrations']")
             .refreshUntilElementVisible("xpath", "//h1[contains(text(), 'CRMs')]")
+            .refreshUntilElementVisible("xpath", "//h1[contains(text(), 'CRMs')]")
             .refreshUntilElementVisible("xpath", "//h1[contains(text(), 'Phone Systems')]");
+        */
     },
 
-    PC_ChildPartner_End : function(client) {
+    PC_IntegrationsEnd : function(client) {
         client.end();
     }
   }; 
