0403c2133bdb8f3506a4853f93dcd48f1a5f82f4
Karol Kotwica <karol.kotwica@britenet.com.pl>
2018-05-15 13:58:45 +0200

    chaining methods

--- tests/tests/PC_IntegrationTest.js
+++ tests/tests/PC_IntegrationTest.js
@@ -1,13 +1,13 @@
 module.exports = {
 
     PC_IntegrationsStart : function(client) {
-        client.loginToTenfold();
-        client.goToDestination(false, 'Integrations');
-            // .refreshUntilElementVisible("xpath", "//nav[@class='forceCommunityNavigationMenu']")
-            //.click("//a[@title = 'Integrations']")        
         client
+            .loginToTenfold()
+            .goToDestination(false, 'Integrations')
             .refreshUntilElementVisible("xpath", "//h1[contains(text(), 'CRMs')]")
             .refreshUntilElementVisible("xpath", "//h1[contains(text(), 'Phone Systems')]");
+            // .refreshUntilElementVisible("xpath", "//nav[@class='forceCommunityNavigationMenu']")
+            //.click("//a[@title = 'Integrations']")
     },
 
     PC_IntegrationsEnd : function(client) {
