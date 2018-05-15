d4532975b995b41c6517e004282d7a16f9a476f4
Karol Kotwica <karol.kotwica@britenet.com.pl>
2018-05-15 15:24:11 +0200

    trying to activate screenshots on failure

--- tests/tests/PC_IntegrationTest.js
+++ tests/tests/PC_IntegrationTest.js
@@ -1,11 +1,13 @@
 module.exports = {
 
     PC_IntegrationsStart : function(client) {
+        var testDuration2 = {maxRounds: 1, timeout: 3000};
+
         client
             .loginToTenfold()
             .goToDestination(false, 'Integrations')
-            .refreshUntilElementVisible("xpath", "//h1[contains(text(), 'CRMs')]")
-            .refreshUntilElementVisible("xpath", "//h1[contains(text(), 'Phone Systems')]");
+            .refreshUntilElementVisible("xpath", "//h1[contains(text(), 'CRMs')]", testDuration2)
+            // .refreshUntilElementVisible("xpath", "//h1[contains(text(), 'Phone Systems')]", testDuration2);
             // .refreshUntilElementVisible("xpath", "//nav[@class='forceCommunityNavigationMenu']")
             //.click("//a[@title = 'Integrations']")
     },
